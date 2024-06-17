655977f965f31d0ed35d44fbfa02b251620c8342
saefulbarkah <saefulbarkah650@gmail.com>
2024-06-17 09:17:00 +0700

    feat: Resonance nexus and beacon

--- development/CustomTP/Client/Content/Aki/JavaScript/Game/Manager/ModFuncs/ModTpFile.js
+++ development/CustomTP/Client/Content/Aki/JavaScript/Game/Manager/ModFuncs/ModTpFile.js
@@ -12,18 +12,23 @@ const { VoidThunderFarm } = require('./function/c4-void-thunder');
 const { C4SupportFarm } = require('./function/c4-support-farm');
 const { C3VoidThunderFarm } = require('./function/c3-void-thunder');
 const { C3MoonlightFarm } = require('./function/c3-moonlight-farm');
+const { ResonanceBeacon, ResonanceNexus } = require('./data/waypoint');
 
 class ModTpFile {
   static C4_VoidThunderLoop50 = VoidThunderFarm(50);
   static C4_SupportLoop50 = C4SupportFarm(50);
   static C3_VoidThunderFarm = C3VoidThunderFarm();
   static C3_MoonlightFarm = C3MoonlightFarm();
+  static ResonanceBeacon = ResonanceBeacon;
+  static ResonanceNexus = ResonanceNexus;
 
   static CustomTpList = [
     this.C4_VoidThunderLoop50,
     this.C4_SupportLoop50,
     this.C3_VoidThunderFarm,
     this.C3_MoonlightFarm,
+    this.ResonanceBeacon,
+    this.ResonanceNexus,
   ];
 }
 exports.ModTpFile = ModTpFile;
